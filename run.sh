d9017fb15658821593a3a12eb8e3c64724aeb23e
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2020-02-05 10:58:12 -0500

    fead: create log of training

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/pjreddie/darknet.git
 cd darknet && make
 cd ..
-./darknet/darknet detector train custom_data/detector.data custom_data/cfg/yolov3.cfg custom_data/weights/darknet53.conv.74
+./darknet/darknet detector train custom_data/detector.data custom_data/cfg/yolov3.cfg custom_data/weights/darknet53.conv.74 > train.log
